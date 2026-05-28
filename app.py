68d090a54925ccfdb07894aa97bff8f7849c21a2
vinsmoke229 <elleira072@gmail.com>
2026-05-28 15:14:32 +0000

    feat : add function multiplication

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ def add(a, b):
 if __name__ == "__main__":
     print("--- Ma Super Toolbox ---")
     print(f"Résultat addition : {add(5, 5)}")
+
+def multiply(a, b):
+    return a * 
+
+def multiply(a, b):
+    return a * b
