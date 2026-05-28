f9cc168f6a2fec6aec08cb9a2b4ef826f6b33c3d
ComlanKokou <armel.comlan@etu.iai-togo.tg>
2026-05-28 15:24:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ def add(a, b):
 
 if __name__ == "__main__":
   
+def add(a, b):
+    return a + b
+
+if __name__ == "__madef add(a, b):
+    return a + b
+
+if __name__ == "__madef add(a, b):
+    return a + b
+
+if __name__ == "__madef add(a, b):
+    return a + b
+
+if __name__ == "__ma
